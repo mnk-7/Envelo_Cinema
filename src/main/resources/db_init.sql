756f7ff75e1e29b80f330d0198a5a6c48d9e328d
mnk7 <mnk7.github@gmail.com>
2023-02-09 18:59:56 +0100

    Adding DB test entries
    Adding test data for AgeRestriction and Genres. Adjusting application.properties file.

---
+++ src/main/resources/db_init.sql
@@ -0,0 +1,22 @@
+//genres
+INSERT INTO genres (name) VALUES ('horror');
+INSERT INTO genres (name) VALUES ('thriller');
+INSERT INTO genres (name) VALUES ('action');
+INSERT INTO genres (name) VALUES ('comedy');
+INSERT INTO genres (name) VALUES ('drama');
+INSERT INTO genres (name) VALUES ('documentary');
+INSERT INTO genres (name) VALUES ('adventure');
+INSERT INTO genres (name) VALUES ('historical');
+INSERT INTO genres (name) VALUES ('fantasy');
+INSERT INTO genres (name) VALUES ('science fiction');
+INSERT INTO genres (name) VALUES ('musical');
+INSERT INTO genres (name) VALUES ('romance');
+INSERT INTO genres (name) VALUES ('western');
+
+
+//age restrictions
+INSERT INTO age_restrictions (min_age) VALUES ('all');
+INSERT INTO age_restrictions (min_age) VALUES ('6');
+INSERT INTO age_restrictions (min_age) VALUES ('12');
+INSERT INTO age_restrictions (min_age) VALUES ('15');
+INSERT INTO age_restrictions (min_age) VALUES ('18');
